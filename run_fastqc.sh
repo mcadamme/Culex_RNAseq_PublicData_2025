5301455f0dc234bd8a8cbd09d28c53edbe752d37
Megan <mfritz13@umd.edu>
2025-05-27 14:03:29 -0400

    bash now recognizing wildcard in file names

--- run_fastqc.sh
+++ run_fastqc.sh
@@ -3,12 +3,35 @@
 #This is the script I used to run fastqc on the RNAseq data from the SRA
 #MLF 05262025
 
-for file in ~/Desktop/Public_mosquito_rnaseq/SRR*/fastq
 
-do
+#!/bin/bash
 
-	/home/megan/src/FastQC/fastqc *1.fastq.gz
-	/home/megan/src/FastQC/fastqc *2.fastq.gz
+# Set the base directory
+BASE_DIR="/home/megan/Desktop/Public_mosquito_rnaseq"
 
+# Loop through the first and second level directories
+for first_level in "$BASE_DIR"/SRR*/; do
+    for second_level in "$first_level"*/; do
+        # Run fastqc on each file in each directory
+            /home/megan/src/FastQC/fastqc *.fastq.gz
+
+    done
+done
+
+
+
+# Setting the base directory
+BASE_DIR="/home/megan/Desktop/Public_mosquito_rnaseq"
+
+for first_level in "$BASE_DIR"/SRR*/; do
+    for second_level in "$first_level"*/; do
+        
+        # Expand both files
+        files=("${second_level}"*.fastq.gz)
+        
+        # Run fastqc on each file in this directory
+            /home/megan/src/FastQC/fastqc "${files}"
+    
+    done
 done
 
