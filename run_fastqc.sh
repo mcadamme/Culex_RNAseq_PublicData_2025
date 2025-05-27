3406a6890b1b369cefaebb166d73fbd5391ac5ff
Megan <mfritz13@umd.edu>
2025-05-27 14:16:54 -0400

    final script running fastqc for analysis

--- run_fastqc.sh
+++ run_fastqc.sh
@@ -3,23 +3,6 @@
 #This is the script I used to run fastqc on the RNAseq data from the SRA
 #MLF 05262025
 
-
-#!/bin/bash
-
-# Set the base directory
-BASE_DIR="/home/megan/Desktop/Public_mosquito_rnaseq"
-
-# Loop through the first and second level directories
-for first_level in "$BASE_DIR"/SRR*/; do
-    for second_level in "$first_level"*/; do
-        # Run fastqc on each file in each directory
-            /home/megan/src/FastQC/fastqc *.fastq.gz
-
-    done
-done
-
-
-
 # Setting the base directory
 BASE_DIR="/home/megan/Desktop/Public_mosquito_rnaseq"
 
