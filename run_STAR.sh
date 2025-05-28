ffcd283799f0fe13968e3efdc6797b86b1ae8eae
Megan <mfritz13@umd.edu>
2025-05-28 18:05:13 -0400

    moved results to new folder so as not to overwrite

--- run_STAR.sh
+++ run_STAR.sh
@@ -24,15 +24,15 @@ for first_level in "$BASE_DIR"/SRR*/; do
     	INFILE1="${second_level}${SUBDIR_NAME}"_trimmed_R1_paired.fastq.gz
     	INFILE2="${second_level}${SUBDIR_NAME}"_trimmed_R2_paired.fastq.gz
     
-
 	#Run Mapping Job with ENCODE standard options
-
 	/home/megan/src/STAR-2.7.11b/source/STAR --runThreadN 7 --genomeDir /home/megan/Desktop/Public_mosquito_rnaseq/Genome \
 	--readFilesCommand gunzip -c --readFilesIn "${INFILE1}" "${INFILE2}" --outFilterType BySJout --outFilterMultimapNmax 20 \
 	--alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 \
 	--alignIntronMax 1000000 --alignMatesGapMax 1000000
 
-
+	mv Aligned.out.sam "${second_level}${SUBDIR_NAME}".sam
+	cat Log.final.out >> Log_summary
+	
 	done
 done
 
