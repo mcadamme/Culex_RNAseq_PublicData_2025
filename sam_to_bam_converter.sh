792955388171d10f1921eaf5015c9de890c58073
Megan <mfritz13@umd.edu>
2025-05-29 11:24:50 -0400

    first draft script used to convert sam files to bam file format needed for htseqcount

---
+++ sam_to_bam_converter.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+#This is the script I used for sam to bam conversion
+#MF 05292025
+
+#Setting the base directory
+BASE_DIR="/home/megan/Desktop/Public_mosquito_rnaseq"
+
+for first_level in "$BASE_DIR"/SRR*/; do
+     
+     for second_level in "$first_level"*/; do
+     
+     cd "${second_level}"
+    
+    
+     #getting SRR name
+     SUBDIR_NAME=$(basename "$(dirname "${second_level}")")
+     
+     for sample in "${SUBDIR_NAME}".sam; do
+     echo "$sample"
+     
+     #convert file from SAM to BAM format
+     samtools view -bS $sample -o ${SUBDIR_NAME}.uns.bam
+     
+     #Sort BAM file
+     samtools sort ${SUBDIR_NAME}.uns.bam ${SUBDIR_NAME}.bam
+     
+     #Index BAM file
+     samtools index ${SUBDIR_NAME}.bam
+     
+     #Remove intermediate files
+     rm ${SUBDIR_NAME}.uns.bam
+     
+     cd ~
+	   
+	   done
+     done
+done
